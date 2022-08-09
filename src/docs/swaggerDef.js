1ebbde167882166f03196d2be7b154823b583062
Phuong Thanh <phuong74200@gmail.com>
2022-08-09 18:31:38 +0700

    update swagger

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -8,7 +8,7 @@ const swaggerDef = {
         version,
         license: {
             name: 'MIT',
-            url: 'https://github.com/hagopj13/node-express-boilerplate/blob/master/LICENSE',
+            url: 'https://github.com/phuong74200/battle-rode-be',
         },
     },
     servers: [
