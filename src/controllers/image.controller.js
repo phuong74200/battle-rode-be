80e0accf0bdb58bf2af3364592b8c50958380f18
Phuong Thanh <phuong74200@gmail.com>
2022-08-17 18:26:54 +0700

    send base/64 image

--- src/controllers/image.controller.js
+++ src/controllers/image.controller.js
@@ -2,18 +2,13 @@ const httpStatus = require('http-status');
 const appRoot = require('app-root-path');
 const catchAsync = require('../utils/catchAsync');
 const ApiError = require('../utils/ApiError');
+const { imageService } = require('../services');
+const logger = require('../config/logger');
 
 const getImage = catchAsync(async (req, res, next) => {
     const { imageId } = req.params;
-    res.sendFile(
-        `${appRoot}/upload/images/${imageId}`,
-        {
-            headers: { 'Content-Type': 'image/png' },
-        },
-        (err) => {
-            if (err) next(new ApiError(httpStatus.NOT_FOUND, 'image not found'));
-        }
-    );
+    const file = await imageService.getImage(imageId);
+    return res.send(file);
 });
 
 module.exports = {
