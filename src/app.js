b99523495ba53bd4c9b05463feb3cfdd6c8daebb
Phuong Thanh <phuong74200@gmail.com>
2022-08-18 04:31:18 +0700

    be update

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const compression = require('compression');
 const cors = require('cors');
 const passport = require('passport');
 const httpStatus = require('http-status');
-const path = require('path');
+const appRoot = require('app-root-path');
 const config = require('./config/config');
 const morgan = require('./config/morgan');
 const { jwtStrategy } = require('./config/passport');
@@ -55,11 +55,6 @@ if (config.env === 'production') {
 
 // v1 api routes
 app.use('/v1', routes);
-
-// public
-const publicPath = path.join(__dirname, '../public');
-app.use(express.static(publicPath));
-app.use('*', express.static(publicPath));
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
