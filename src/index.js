cbb55dbb76303060126a4436be2bbc190a3c7c09
Phuong Thanh <phuong74200@gmail.com>
2022-08-18 00:00:55 +0700

    prod SSL copy dir

--- src/index.js
+++ src/index.js
@@ -6,21 +6,19 @@ const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
 
-const privateKey = fs.readFileSync('./cert/private.pem', 'utf8');
-const certificate = fs.readFileSync('./cert/origin.pem', 'utf8');
-
-const credentials = { key: privateKey, cert: certificate };
-
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
     logger.info('Connected to MongoDB');
-    const httpsServer = https.createServer(credentials, app);
-    const httpServer = http.createServer(app);
     if (config.env === 'production') {
+        const privateKey = fs.readFileSync('./cert/private.pem', 'utf8');
+        const certificate = fs.readFileSync('./cert/origin.pem', 'utf8');
+        const credentials = { key: privateKey, cert: certificate };
+        const httpsServer = https.createServer(credentials, app);
         server = httpsServer.listen(config.port, () => {
             logger.info(`SSL: Listening to port ${config.port}`);
         });
     } else {
+        const httpServer = http.createServer(app);
         server = httpServer.listen(config.port, () => {
             logger.info(`NoSSL: Listening to port ${config.port}`);
         });
